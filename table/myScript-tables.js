7bf753de7687f36265c64c58aac5a9a5801a0354
Dragan Jelic <73564920+gidra-web@users.noreply.github.com>
2022-01-14 16:31:29 +0100

    Add files via upload

--- table/myScript-tables.js
+++ table/myScript-tables.js
@@ -8,11 +8,11 @@ $(document).ready(function () {
             var tableData = "";
             var todoTable = document.getElementById("todoTable");
             $.each(myJson, function (index, todoItem) {
-                tableData += `<tr><th class="text-bold">${todoItem.postId}</th>
-                <td>${todoItem.id} </td>
-                <td>${todoItem.name}</td>
-                <td>${todoItem.email}</td>
-                <td><a href="#" class="btn btn-success" style="width:160px" onmouseover="alert(' ${ShowMessage(todoItem.body)} ');">Show Message</a></td></tr>;`
+                tableData += '<tr><th class="text-bold">' + todoItem.postId + '</th><td>' +
+                    todoItem.id + '</td><td>' +
+                    todoItem.name + '</td><td>' +
+                    todoItem.email + '</td>' +
+                    '<td><a href="#" class="btn btn-success" style="width:160px;" onclick="alert(`' + todoItem.body + '`);">Show Message</a></td></tr>';
             });
             todoTable.innerHTML = tableData;
         });
